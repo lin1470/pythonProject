53a7398562976db68a64fceda9318570aac10229
15zlli2@stu.edu.cn <15zlli2@stu.edu.cn>
2018-12-10 11:36:46 +0800

    test

---
+++ Obeject-oriented/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Obeject-oriented',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='bruce',
+    author_email='',
+    description=''
+)
