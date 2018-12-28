fb07c6538c4b31966ee7d4800e75859418eb928e
15zlli2@stu.edu.cn <15zlli2@stu.edu.cn>
2018-12-28 13:41:10 +0800

    添加新功能

--- xmlTrans/Transformer.py
+++ xmlTrans/Transformer.py
@@ -4,6 +4,7 @@ import os
 import csv
 from pretty_process import *
 from util import *
+import json
 
 
 # 自定义一个转换的代码类
@@ -16,6 +17,15 @@ class Transformer():
         self.configPath = "config.csv"
         self.dcData = None
         self.modsData = None
+        self.dcdirectory = None
+        self.modstemplate = None
+        self.loadInput()
+
+    def setDcdir(self,path):
+        self.dcdirectory = path
+
+    def setModsTem(self,path):
+        self.modstemplate = path
 
     def setOutputPath(self,path):
         self.outputPath = path
@@ -43,7 +53,19 @@ class Transformer():
         self.dcData = ET.parse(self.dcPath)
         self.modsData = ET.parse(self.modsPath)
 
+# 判断输入的文件夹和mods的模板是否符合规范(后续继续添加判断数据)
+    def checkPath(self):
+        if not os.path.isdir(self.dcdirectory) or not os.path.exists(self.modstemplate):
+            return False
+
+        return True
+
+
     def transform(self):
+        # 先检查文件路径
+        if not self.checkPath():
+            print("the wrong dc directory or modstemplate")
+            return
         self.preProcess()
         self.loadConfig()
         self.loadData()
@@ -66,7 +88,7 @@ class Transformer():
                 #     print(dctext)
             for item in modsroot.iter(modsword):
                 item.text = dctext
-        self.modsData.write(self.outputPath)
+        self.modsData.write(self.outputPath,xml_declaration=True)
         self.afterProcess()
 
     def preProcess(self):
@@ -74,6 +96,15 @@ class Transformer():
 
     def afterProcess(self):
         afterProcess()
+
+# 先载入dc文件夹的路径和mods的模板
+    def loadInput(self):
+        inputdata = None
+        with open("input.json",'rb') as f:
+            inputdata = json.load(f)
+        print(inputdata)
+        self.setDcdir(inputdata['dcdirectory'])
+        self.setModsTem(inputdata['modstemplate'])
 
 
 if __name__ == '__main__':
